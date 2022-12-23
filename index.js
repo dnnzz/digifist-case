33a847884b633fb29aba85a8a2843666c90948f3
dnzz <deniz.firat@akinon.com>
2022-12-23 17:28:04 +0300

    mobile & slider button

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const leftBtn = document.getElementById('left-btn');
+const hexDots = document.querySelectorAll('.hex-dot');
+const mobileHexDots = document.querySelectorAll('.mobile-hex-dot');
 
 leftBtn.addEventListener('click', () => {
   document.getElementById('scroll-content').scrollLeft += 100;
 });
+
+hexDots.forEach(dot => {
+  dot.addEventListener('click', (e) => clickCb(e,hexDots));
+});
+
+mobileHexDots.forEach(dot => {
+  dot.addEventListener('click', (e) => clickCb(e,mobileHexDots));
+});
+
+function clickCb(e, dotsNodeArr) {
+    let pk = e.target.getAttribute('pk');
+    removeAllDots(pk,dotsNodeArr);
+    e.target.classList.add('--selected');
+}
+
+function removeAllDots(pk,dotsNodeArr) {
+  dotsNodeArr.forEach(dot => {
+    if(dot.getAttribute('pk') == pk){
+      dot.classList.remove('--selected');
+    }
+  });
+}
